f7d2d23fda3782537b4953d3c4e3df4c605e7fc8
kaseOga <galindero@gmail.com>
2015-11-08 21:23:11 +0100

    Testing integration test in user repository

--- main/spc/intr/domain/user/UserRepository.spec.ts
+++ main/spc/intr/domain/user/UserRepository.spec.ts
@@ -6,17 +6,16 @@ import {User, CreateNewUser} from '../../../../src/domain/user/User';
 import {UserRepository} from '../../../../src/domain/user/UserRepository';
 
 describe("UserRepository", () => {
-    it("should create a new user", eventually (async () => {
+    it("should create a new user", (async () => {
         await SharedConnectionRepository.connect();
         const user = CreateNewUser("testUser", "valid@email.com", "testPassword");
         const repo = new UserRepository();
+        await repo.drop();
+
         await repo.save(user);
 
-        const userExistPromise = repo.findByEmail('valid@test.com');
+        await expect(repo.findByEmail('valid@email.com')).to.eventually.eq(user);
 
-        userExistPromise.then((user) => {
-            expect(user).to.be.eq(user);
-        });
         await SharedConnectionRepository.disconnect();
     }));
 });
