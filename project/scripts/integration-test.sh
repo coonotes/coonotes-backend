a6a9d4353430e5420f64327fadb4eb269245d018
kaseOga <galindero@gmail.com>
2015-11-26 22:37:01 +0100

    Use dot-matrix reporter to mocha

--- project/scripts/integration-test.sh
+++ project/scripts/integration-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./node_modules/gobble-cli/lib/index.js build tmp -f
-./node_modules/mocha/bin/_mocha tmp/spc/intr/**/**/**/**/**/**/**/*.spec.js
+./node_modules/mocha/bin/_mocha -R dot tmp/spc/intr/**/**/**/**/**/**/**/*.spec.js
--- project/scripts/unit-test.sh
+++ project/scripts/unit-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./node_modules/gobble-cli/lib/index.js build tmp -f
-./node_modules/mocha/bin/_mocha tmp/spc/unit/**/**/**/**/**/**/**/*.spec.js
+./node_modules/mocha/bin/_mocha -R dot tmp/spc/unit/**/**/**/**/**/**/**/*.spec.js
 
